f5352a648b675ead5221f60854fb583751f89ead
Stonewall1 <impression97p@gmail.com>
2022-05-14 20:42:49 +0300

    Task6

---
+++ src/Task6.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Task6 {
+    public static void main(String[] args) {
+        String[] names = {"Nasty" , "Dmitry" , "Ilya" , "Roman" , "Vlad" , "5"  , "Julia" , "Maria" , "aaa"  , "R2D2" };
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+    }
+}
